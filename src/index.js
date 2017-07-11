2f45ae863b3c68f43f09d88d924f69cc573f335e
Thierry Michel <thmichel@gmail.com>
2017-07-11 12:51:43 +0200

    :ok_hand: update main script to make it compatible with vue.js2 Thanks to @lovethebomb https://github.com/thierrymichel/vue-svg-sprite/pull/1

--- src/index.js
+++ src/index.js
@@ -1,36 +1,35 @@
+/* eslint-disable no-magic-numbers */
 export default {
   install(Vue, opts = {}) {
-
     const dir = {
       params: ['symbol', 'size'],
-      bind() {
-
+      bind(el) {
         // Get options
         opts.class = opts.class || 'icon';
         opts.url = opts.url || '/assets/svg/sprite.svg';
 
         // Get params
         // If expression + "symbol" param -> use expression
-        let id = this.expression || this.params.symbol;
-        let size = this.params.size;
+        const id = this.expression || this.params.symbol;
+        let { size } = this.params;
 
-        // Get element
-        let el = this.el;
         // Set viewBox, widht, height attributes ?
         if (size) {
           // Normalize valid separators: / /, /,/
           size = size.replace(/( |, |,)/g, ' ');
-          let sizeValues = size.split(' ');
-          let l = sizeValues.length;
-          let viewBox = [];
+          const sizeValues = size.split(' ');
+          const l = sizeValues.length;
+          const viewBox = [];
 
           if (l === 3 || l > 4) {
-            console.warn('[vue-svg-sprite] size: ', size,' is not valid');
+            console.warn('[vue-svg-sprite] size: ', size, ' is not valid');
           } else {
-            viewBox[0] = (l < 4) ? 0 : sizeValues[0];
-            viewBox[1] = (l < 4) ? 0 : sizeValues[1];
-            let w = viewBox[2] = (l < 4) ? sizeValues[0] : sizeValues[2];
-            let h = viewBox[3] = sizeValues[l - 1];
+            viewBox[0] = l < 4 ? 0 : sizeValues[0];
+            viewBox[1] = l < 4 ? 0 : sizeValues[1];
+            /* eslint-disable no-multi-assign */
+            const w = viewBox[2] = l < 4 ? sizeValues[0] : sizeValues[2];
+            const h = viewBox[3] = sizeValues[l - 1];
+            /* eslint-enable no-multi-assign */
 
             el.setAttribute('viewBox', viewBox.join(' '));
             el.setAttribute('width', w);
@@ -52,8 +51,9 @@ export default {
         }
 
         // Add the <use> element to <svg>
-        let href = opts.url + '#' + id;
-        let use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+        const href = `${opts.url}#${id}`;
+        const use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+
         use.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', href);
         el.appendChild(use);
       },
